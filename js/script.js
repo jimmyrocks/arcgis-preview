ed8f11c5c1979df9c4c8efb561e90b5528c9274b
Jim McAndrew <jim@loc8.us>
2018-02-02 05:45:03 -0500

    Point to ArcGIS endpoints

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+/* global $, L, esriToGeoJSON, CodeMirror */
 (function(){
-  'use strict'
+  'use strict';
   //initialize a leaflet map
   var map = L.map('map', {minZoom: 0, maxZoom: 21})
     .setView([40.708816,-74.008799], 11);
@@ -7,11 +8,9 @@
   //layer will be where we store the L.geoJSON we'll be drawing on the map
   var layer;
 
-  var sql;
-
   //add CartoDB 'dark matter' basemap
   L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
   }).addTo(map);
 
   var queryHistory = (localStorage.history) ? JSON.parse(localStorage.history) : [];
@@ -39,7 +38,7 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
                map.getBounds()._southWest.lat + ',' +
                map.getBounds()._northEast.lng + ',' +
                map.getBounds()._northEast.lat;
-    
+
     //clear the map
     if( map.hasLayer(layer)) {
       layer.clearLayers();
@@ -54,7 +53,7 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
     }
 
     //Strip the trailing slash if there is one
-    var baseUrl = url.replace(/\/{1,}$/,'');
+    var baseUrl = url.replace(/\/{1,}(query)?(\?)?$/,'');
     var queryObj = {
       'url': baseUrl,
       'where':where,
@@ -74,57 +73,58 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
     $('#notifications').hide();
     $('#download').hide();
     $('#run').addClass('active');
-
     clearTable();
 
-
     // pass the query to the sql api endpoint
-    $.getJSON('/get?', createQueryObj(true), function(data) {
-      console.log('dataz', data);
-      $('#run').removeClass('active');
-      $('#notifications').show();
-      $('#download').show();
-      if (data.error !== undefined) {
-        //write the error in the sidebar
-        $('#notifications').removeClass().addClass('alert alert-danger');
-        $('#notifications').text(typeof data.error === 'string' ? data.error : JSON.stringify(data.error));
-      } else if (data.features.length == 0) {
-        $('#notifications').removeClass().addClass('alert alert-warning');
-        $('#notifications').text('Your query returned no features.');
-      } else {
-        var features = data.features;
-        var geoFeatures = features.filter(function(feature) {
-          return feature.geometry;
-        });
+    var queryObj = createQueryObj(true);
+    var baseUrl = queryObj.url;
+    delete queryObj.url;
+    $.ajax({
+      type: 'POST',
+      dataType: 'jsonp',
+      url: baseUrl + '/query?',
+      data: queryObj,
+      success: function(data) {
+        var features = esriToGeoJSON(data).features;
         $('#notifications').removeClass().addClass('alert alert-success');
-        if (geoFeatures.length) {
-          addLayer(geoFeatures); //draw the map layer
+        if (features.length) {
+          addLayer(features); //draw the map layer
           $('#notifications').text(features.length + ' features returned.');
+          buildTable(features); //build the table
         } else {
+          if (data.error) {
+            $('#notifications').removeClass().addClass('alert alert-danger');
+            $('#notifications').html('Error: ' + JSON.stringify(data.error, null, 2));
+          } else {
           // There is no map to display, so switch to the data view
-          $('#notifications').html(features.length+ ' features returned.<br/>No geometries returned, see the <a href="#" class="data-view">data view</a> for results.');
+            $('#notifications').html(features.length+ ' features returned.<br/>No geometries returned, see the <a href="#" class="data-view">data view</a> for results.');
+          }
           //toggle map and data view
           $('a.data-view').click(function() {
             $('#map').hide();
             $('#table').show();
           });
-
         }
-        buildTable(features); //build the table
+        $('#notifications').show();
+        $('#run').removeClass('active');
+      },
+      error: function(XMLHttpRequest, textStatus) {
+        //write the error in the sidebar
+        $('#run').removeClass('active');
+        $('#notifications').removeClass().addClass('alert alert-danger');
+        $('#notifications').text(textStatus);
+        $('#notifications').show();
       }
-    }, function(e) {
-      $('#notifications').removeClass().addClass('alert alert-danger');
-      $('#notifications').text('unknown error');
     });
-  };
+  }
 
   //toggle map and data view
-  $('.btn-group button').click(function(e) {
+  $('.btn-group button').click(function() {
     $(this).addClass('active').siblings().removeClass('active');
 
     var view = $(this)[0].innerText;
 
-    if(view == "Data View") {
+    if(view == 'Data View') {
       $('#map').hide();
       $('#table').show();
     } else {
@@ -150,14 +150,14 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
     var queryObj = createQueryObj(false);
     queryObj.format = 'geojson';
     var url = '/get?' + urlify(queryObj);
-    window.open(url, '_blank');
+    // window.open(url, '_blank');
   });
 
   $('#csv').click(function() {
     var queryObj = createQueryObj(false);
     queryObj.format = 'csv';
     var url = '/get?' + urlify(queryObj);
-    window.open(url, '_blank');
+    // window.open(url, '_blank');
   });
 
   // initialize keyboard shortcut for submit
@@ -175,14 +175,14 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
       properties = {};
     }
 
-    var table = $("<table><tr><th>Column</th><th>Value</th></tr></table>");
+    var table = $('<table><tr><th>Column</th><th>Value</th></tr></table>');
     var keys = Object.keys(properties);
     var banProperties = ['geom'];
     for (var k = 0; k < keys.length; k++) {
       if (banProperties.indexOf(keys[k]) === -1) {
-        var row = $("<tr></tr>");
-        row.append($("<td></td>").text(keys[k]));
-        row.append($("<td></td>").text(properties[keys[k]]));
+        var row = $('<tr></tr>');
+        row.append($('<td></td>').text(keys[k]));
+        row.append($('<td></td>').text(properties[keys[k]]));
         table.append(row);
       }
     }
@@ -191,38 +191,42 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
 
   function addLayer( features ) {
     //create an L.geoJson layer, add it to the map
-      layer = L.geoJson(features, {
-        style: {
-            color: '#fff', // border color
-            fillColor: 'steelblue',
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.7
-        },
+    layer = L.geoJson(features, {
+      style: {
+        color: '#fff', // border color
+        fillColor: 'steelblue',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.7
+      },
 
-        onEachFeature: function ( feature, layer ) {
-          if (feature.geometry.type !== 'Point') {
-            layer.bindPopup(propertiesTable(feature.properties));
-          }
-        },
-
-        pointToLayer: function ( feature, latlng ) {
-          return L.circleMarker(latlng, {
-            radius: 4,
-            fillColor: "#ff7800",
-            color: "#000",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-          }).bindPopup(propertiesTable(feature.properties));
+      onEachFeature: function ( feature, layer ) {
+        if (feature.geometry.type !== 'Point') {
+          layer.bindPopup(propertiesTable(feature.properties));
         }
-      }).addTo(map)
+      },
 
-      map.fitBounds(layer.getBounds());
-      $('#notifications').empty();
+      pointToLayer: function ( feature, latlng ) {
+        return L.circleMarker(latlng, {
+          radius: 4,
+          fillColor: '#ff7800',
+          color: '#000',
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+        }).bindPopup(propertiesTable(feature.properties));
+      }
+    }).addTo(map);
+
+    map.fitBounds(layer.getBounds());
+    $('#notifications').empty();
   }
 
   function buildTable( features ) {
+    var viewTable = '<div id="tableWrapper"><table id="viewTable" class="table table-striped table-bordered" cellspacing="0"><thead></thead><tfoot></tfoot><tbody></tbody></table></div>';
+    $('#viewTable').remove();
+    $('#table').append(viewTable);
+    console.log('redrawing');
     //assemble a table from the geojson properties
 
     //first build the header row
@@ -233,7 +237,7 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
 
     fields.forEach( function( field ) {
       $('#table').find('thead').find('tr').append('<th>' + field + '</th>');
-      $('#table').find('tfoot').find('tr').append('<th>' + field + '</th>')
+      $('#table').find('tfoot').find('tr').append('<th>' + field + '</th>');
     });
 
     features.forEach( function( feature ) {
@@ -241,20 +245,27 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
       var $tr = $('<tr/>');
 
       fields.forEach( function( field ) {
-        $tr.append('<td>' + feature.properties[field] + '</td>')
-      })
+        $tr.append('<td>' + (feature.properties[field] || '&nsbp;') + '</td>');
+      });
 
       $('#table').find('tbody').append($tr);
     });
 
-      $('#table>table').DataTable();
+    $('#viewTable>table').DataTable();
   }
 
   function clearTable() {
-    $('#table').find('thead').empty();
-    $('#table').find('tfoot').empty();
-    $('#table').find('tbody').empty();
-  };
+    $('#viewTable').remove()
+    // $('#table').find('thead').empty();
+    // $('#table').find('tfoot').empty();
+    // $('#table').find('tbody').empty();
+    // try {
+    //   // $('#table>table').DataTable().clear();
+    // } catch (e) {
+    //   return e;
+    // }
+    // return undefined;
+  }
 
   function addToHistory(entry) {
     //only store the last 25 queries
@@ -269,30 +280,30 @@ attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreet
   }
 
   function updateEntry(entry) {
-    endpoint.setValue(entry.url);
-    editor.setValue(entry.where);
+    window.endpoint.setValue(entry.url);
+    window.editor.setValue(entry.where);
     // bbox.setValue(entry.bbox);
   }
 
   //enable and disable history buttons based on length of queryHistory and historyIndex
   function updateHistoryButtons() {
     if (historyIndex > queryHistory.length - 2) {
-       $('#history-next').addClass('disabled')
-     } else {
-       $('#history-next').removeClass('disabled')
+      $('#history-next').addClass('disabled');
+    } else {
+      $('#history-next').removeClass('disabled');
     }
 
     if(queryHistory[historyIndex-1]) {
-      $('#history-previous').removeClass('disabled')
+      $('#history-previous').removeClass('disabled');
     } else {
-      $('#history-previous').addClass('disabled')
+      $('#history-previous').addClass('disabled');
     }
   }
 
 }());
 
 //Load codemirror for syntax highlighting
-window.onload = function() {            
+window.onload = function() {
   window.endpoint = CodeMirror.fromTextArea(document.getElementById('endpointPane'), {
     indentWithTabs: true,
     smartIndent: true,
@@ -302,7 +313,7 @@ window.onload = function() {
     lineWrapping: true,
     theme: 'monokai'
   });
-  endpoint.setSize(null,115);
+  window.endpoint.setSize(null,115);
 
   window.editor = CodeMirror.fromTextArea(document.getElementById('wherePane'), {
     mode: 'text/x-pgsql',
@@ -315,8 +326,7 @@ window.onload = function() {
     theme: 'monokai'
   });
   $('.CodeMirror')[1].setAttribute('style','top:-14px');
-  editor.replaceRange('\n', {line:2,ch:0}); // create newline for editing
-  editor.setCursor(2,0);
+  window.editor.setSize(null,165);
 
-  window.useExtent = $("input#useExtent")[0];
+  window.useExtent = $('input#useExtent')[0];
 };
